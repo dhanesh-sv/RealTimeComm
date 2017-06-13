067008bbff48e43965e3dd0b9462e5a65676b790
DhaneshS <dhanesh.sv@gmail.com>
2017-06-13 19:20:27 +0530

    major work done

--- httpServer.js
+++ httpServer.js
@@ -40,6 +40,25 @@ function clientConnected(client) {
     client.on("addUser", addUser);
     function addUser(userData) {
 
+        //checking if user exists or not
+        var userAvailabilityCheck = usersList.filter(function (i) {
+            //console.log(i.value.userId);
+            //console.log(userData.userId);
+            return i.value.userId == userData.userId;
+
+        });
+        //console.log(userAvailabilityCheck);
+        //console.log(userAvailabilityCheck.length);
+
+        //removing existing user details
+        if (userAvailabilityCheck.length > 0) {
+            usersList = usersList.filter(function (i) {
+
+                return i.value.userId !== userData.userId;
+            });
+        };
+
+        console.log(usersList);
         userData.socketId = client.id;
         // usersList[client.id] = userData;
         usersList.push({
@@ -57,6 +76,8 @@ function clientConnected(client) {
         io.sockets.emit("receiveUsersList", usersList);
 
     };
+    //client joining methods
+
 
     //***start****//message section//***start****//
     client.on("clientMessage", clientMessage);
@@ -77,4 +98,45 @@ function clientConnected(client) {
         console.log(usersList);
         io.sockets.emit("receiveUsersList", usersList);
     };
+
+    //***start****//initiate call//***start****//
+    //response codes
+    //1-same user
+    //2-accepted
+    //3-rejected
+    client.on("initiateCallAction", initiateCallAction)
+    function initiateCallAction(toClientId) {
+        var responseCode, responseData;
+        console.log("toClientId-" + toClientId);
+        if (client.id == toClientId) {
+            responseCode = 1;
+            client.emit('responseFromReceiver', responseCode, responseData);
+        };
+
+
+      //var res=  usersList.map(n => {
+      //      if (client.id === n.key) {
+      //          console.log(n.value.userName);
+      //          return n.value.userName;
+      //      }
+      //  });
+
+
+
+        var getCurrentClientObj = usersList.filter(function (i) {
+            return i.key == client.id;
+        });
+
+
+        console.log(getCurrentClientObj[0].key);
+        console.log(getCurrentClientObj[0].value.userId);
+        client.broadcast.to(toClientId).emit('requestingReceiverForCall', client.id, getCurrentClientObj[0].value.userName)
+       // console.log(client.id);
+        //var ids=client.id;
+        //console.log(usersList[ids]);
+        // client.broadcast.to(toClientId).emit('requestingReceiverForCall', client.id,);
+        // io.sockets.emit("responseFromReceiver", responseCode, responseData);
+    };
+    //***end****//initiate call//***end****//
+
 };
