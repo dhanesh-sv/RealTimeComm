375003a683176b174235ed5382f1cc0334e2f146
DhaneshS <dhanesh.sv@gmail.com>
2017-07-13 10:07:09 +0530

    tuning

--- app.js
+++
@@ -1,8 +0,0 @@
-var usersList = [{ key: 1, value: { userid: 1, usename: 'dhanesh' } }
-, { key: 1, value: { userid: 2, usename: 'd2' } }];
-
-usersList = usersList.filter(function (i) {
-    return i.key !== 1;
-});
-
-console.log(usersList);
